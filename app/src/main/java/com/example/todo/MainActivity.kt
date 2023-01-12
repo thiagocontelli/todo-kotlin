f93c0d38e107280cd075601f1445bcba37706e0a
thiagocontelli <thiagocontellid@gmail.com>
2023-01-11 22:35:44 -0300

    .

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,24 +3,26 @@ package com.example.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todo.ui.theme.ToDoTheme
-import java.util.UUID
+import java.util.*
 
-data class Task(val id: UUID, val description: String)
+data class Task(val id: UUID, val description: String, var isSelected: Boolean)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ToDoScreen() {
-    var tasks by remember { mutableStateListOf<Task>() }
+    val tasks = remember { mutableStateListOf<Task>() }
     var task by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
@@ -48,20 +50,36 @@ fun ToDoScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.padding(16.dp))
-        EditTaskField(task, { task = it })
+        EditTaskField(task) { if (it.length <= 100) task = it }
         Spacer(Modifier.padding(8.dp))
         Button(
             {
                 if (task.trim() !== "") {
                     val id = UUID.randomUUID()
-                    val newTask = Task(id, task)
+                    val newTask = Task(id, task, false)
                     tasks.add(newTask)
-                    println(tasks)
+                    task = ""
                 }
             }, modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Add Task")
         }
+        Spacer(Modifier.padding(8.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            itemsIndexed(tasks) { index, item ->
+                TaskCard(item.description, selected = item.isSelected, onClick = {
+                    val taskToBeRemoved = tasks.find { it.id == item.id }
+                    tasks.remove(taskToBeRemoved)
+                }, onSelect = {
+                    val newTask = Task(item.id, item.description, it)
+                    tasks[index] = newTask
+                })
+            }
+
+        }
+
     }
 }
 
@@ -75,6 +93,47 @@ fun EditTaskField(
         modifier = Modifier.fillMaxWidth(),
         label = { Text(stringResource(R.string.text_placeholder)) },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
     )
+    Row(
+        horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()
+    ) { Text(value.length.toString() + "/100", fontSize = 14.sp) }
+}
+
+@Composable
+fun TaskCard(
+    task: String, onClick: () -> Unit, selected: Boolean, onSelect: (Boolean) -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 2.dp,
+        backgroundColor = if (selected) Color.LightGray else Color.White,
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically, modifier = Modifier.weight(0.8f)
+            ) {
+                Checkbox(selected, onSelect)
+                Text(
+                    task,
+                    fontSize = 16.sp,
+                    style = if (selected) TextStyle(textDecoration = TextDecoration.LineThrough) else TextStyle(
+                        textDecoration = TextDecoration.None
+                    ),
+                )
+            }
+            IconButton(
+                onClick, modifier = Modifier.weight(0.2f)
+            ) {
+                Icon(
+                    painterResource(R.drawable.ic_trash), null, tint = Color(0xFFBE1F1F)
+                )
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/todo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todo/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,9 +14,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF118AB2),
+    primaryVariant = Color(0xFF145B72),
+    secondary = Color(0xFF118AB2)
 
     /* Other default colors to override
     background = Color.White,
